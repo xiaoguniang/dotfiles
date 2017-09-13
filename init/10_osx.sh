976531088fbf9e4338ef7563dba79873afc3b17d
Hongbo Liu <hbliu@freewheel.tv>
2017-09-14 01:26:44 +0800

    regular backup

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -4,15 +4,16 @@ is_osx || return 1
 # Some tools look for XCode, even though they don't need it.
 # https://github.com/joyent/node/issues/3681
 # https://github.com/mxcl/homebrew/issues/10245
-if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
-  sudo xcode-select -switch /usr/bin
-fi
+
+# if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
+#   sudo xcode-select -switch /usr/bin
+# fi
 
 # Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Monaco Font
-open ~/.dotfiles/data/Mac/Monaco+for+Powerline.otf
+# open ~/.dotfiles/data/Mac/Monaco+for+Powerline.otf
 
 # Count Lines of Code https://github.com/AlDanial/cloc
 brew tap neovim/neovim
